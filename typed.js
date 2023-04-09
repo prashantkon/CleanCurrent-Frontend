0ced790bdd105a91ab8fe106717bd1a40bf65961
The Galactic Senate <80652317+Old-Respect-9735@users.noreply.github.com>
2023-04-09 05:34:36 -0700

    updated version

--- typed.js
+++
@@ -1,9 +0,0 @@
-var typingEffect = new Typed(".multiText", {
-    strings: ["Cleaning".bold(), "Sorting".bold(), "Improving".bold(), "Saving".bold(), "Innovating".bold(), "Sustaining".bold()],
-    loop: true,
-    typeSpeed: 90,
-    backSpeed: 80,
-    startDelay: 2000,
-    backDelay: 2000,
-    onLastStringBackspaced: (self) => {}
-});
