d77d9bb59446a63ad7c560144fd373a3b22b5174
The Galactic Senate <80652317+Old-Respect-9735@users.noreply.github.com>
2023-04-09 07:16:31 -0700

    everybody but other is here

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 var problemBtns = document.querySelectorAll(".btn.btn-outline-dark.btn-rounded");
 problemBtns = document.getElementsByClassName("btn btn-outline-dark btn-rounded");
-console.log("SDHDS");
 for (let i = 0; i < problemBtns.length; i++) {
     problemBtns[i].onclick = () => {
         location.href = "problem.html";
     };
 }
 
+document.getElementById("productBtn").addEventListener("click", function() {
+    location.href = "product.html";
+});
 document.getElementById("home").addEventListener("click", function() {
     location.href = "index.html";
 });
 document.getElementById("problem").addEventListener("click", function() {
     location.href = "problem.html";
+})
+document.getElementById("product").addEventListener("click", function() {
+    location.href = "product.html";
 })
---
+++ problem.js
@@ -0,0 +1,10 @@
+
+document.getElementById("home").addEventListener("click", function() {
+    location.href = "index.html";
+});
+document.getElementById("problem").addEventListener("click", function() {
+    location.href = "problem.html";
+})
+document.getElementById("product").addEventListener("click", function() {
+    location.href = "product.html";
+})
